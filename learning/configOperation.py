15c35133f229dd391c5fa90dabae70d5ed4e58ec
wildTurtle <hehu_2009@163.com>
2018-06-14 17:13:23 +0800

    learning config

---
+++ learning/configOperation.py
@@ -0,0 +1,42 @@
+# -*- encoding:utf-8-*-
+
+import configparser
+import os
+
+# 获取当前路径、配置文件全路径
+proDir = os.path.split(os.path.realpath(__file__))[0]
+configPath = os.path.join(proDir, "config.ini")
+
+
+class ConfigOperation:
+    cf = configparser.ConfigParser()
+    cf.read(configPath)
+
+    # 定义获取config下的value
+    def getConfigValue(self, name):
+        value = self.cf.get("config", name)
+        return value
+
+    # 定义获取cmd下name的value
+    def getCmdValue(self, name):
+        value = self.cf.get("cmd", name)
+        return value
+
+    # 定义方法，修改config分组下面name的值value
+    def setConfigValue(self, name, value):
+        self.cf.set("config", name, value)
+        fp = open(r'config.ini', 'w')
+        self.cf.write(fp)
+
+    # 定义方法，修改cmd分组下name的值value
+    def setCmdValue(self, name, value):
+        self.cf.set("cmd", name, value)
+        fp = open(r'configPath', 'w')
+        self.cf.write(fp)
+
+
+if __name__ == '__main__':
+    cf = ConfigOperation
+    print(cf.getConfigValue(cf, "appPackage"))
+    cf.setConfigValue(cf, "appPackage", "asdasd")
+    print(cf.getConfigValue(cf, "appPackage"))
