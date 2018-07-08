d1ce3d526b8571e00f147eaf00bf586d3c6beffd
wildTurtle <hehu_2009@163.com>
2018-07-08 22:13:16 +0800

    os

---
+++ learning/dafeiji.py
@@ -0,0 +1,33 @@
+f = open('/Users/guozhen/Documents/123.txt')
+
+boy = []
+girl = []
+count = 1
+
+for each_line in f:
+    if each_line[:6] != '======':
+        #执行分隔符操作
+        (role, line_spoken)= each_line.split('：',1)
+        if role =='小甲鱼':
+           boy.append(line_spoken)
+        if role == '小客服':
+            girl.append(line_spoken)
+    else:
+        file_name_boy = 'boy_' + str(count) +'.txt'
+        file_name_girl = 'girl_' + str(count) + '.txt'
+
+        boy_file = open(file_name_boy,'w')
+        girl_file = open(file_name_girl,'w')
+
+        boy_file.writelines(boy)
+        girl_file.writelines(girl)
+
+        boy_file.close()
+        girl_file.close()
+
+        boy = []
+        girl = []
+        count += 1
+
+
+f.close()
