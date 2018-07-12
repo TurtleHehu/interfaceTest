1d95f904cb4cead4474027c6065557a2e09f3707
wildTurtle <hehu_2009@163.com>
2018-07-12 18:16:05 +0800

    request learning

--- learning/defTest.py
+++ learning/defTest.py
@@ -3,4 +3,4 @@ def f(x):
 
 l = map(f,[1,2,3,4,5,6,7,8,9,10])
 
-print()
+print(l)
---
+++ learning/requestLearning.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+##########################Get请求#######################
+
+# 发送无参数的get请求
+baiDu_response = requests.get('http://www.baidu.com')
+
+#发送无参数的get请求，设置超时时间timeout
+baiDu_response = requests.get('http://www.baidu.com', timeout=1)
+
+#查看请求的url地址
+print('无参数的get请求地址是：'+baiDu_response.url)
+
+#查看当前返回状态码
+print('当前返回状态码是：'+str(baiDu_response.status_code))
+
+#查看当前返回内容
+print('当前返回内容是：'+baiDu_response.text)
+
+#查看当前返回内容字节流
+print('当前返回内容是：'+str(baiDu_response.content))
+
+#带参数的请求
+sendDictParams = {'keys':'alex','age':18}
+baiDu_dictParams_response = requests.get('http://www.baidu.com',params=sendDictParams)
+print('当前返回内容json：'+str(baiDu_dictParams_response.content))
+
+#查看发送请求的url
+print(baiDu_dictParams_response.url)
+print(baiDu_dictParams_response.headers.get('Connection'))
+
+#############cookies####################
+cookies_response = requests.get('http://www.baidu.com')
+print(str(cookies_response.cookies))
+
+cookies = {'user-cookies': 'myCookies'}
+custom_cookies_response = requests.get('http://www.baidu.com',cookies=cookies)
+print(str(custom_cookies_response.cookies))
+with open("百度.html","wb") as html:
+    html.write(custom_cookies_response.content)
+html.close()
